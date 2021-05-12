5f26786eff80cabb5aebf2c2427280bca748b677
Aleksandr Lazarev <aleqqq132@gmail.com>
2021-05-12 08:44:07 +0300

    revisions

--- Homework_13/script1.js
+++ Homework_13/script1.js
@@ -9,29 +9,44 @@ function newInput(e, item, active){
         e.attachEvent(item, active);
 }
 
-var c = document.getElementById('center');
+var mainBlock = document.getElementById('center');
 
-newInput(c, 'mouseup', function(e) {
+newInput(mainBlock, 'mouseup', function(e) {
     if(e.target.tagName.toLowerCase() == 'div')
     {   
         input.value = block.textContent;
         e.target.appendChild(input);
         input.classList.remove('hidden');
         input.focus();
-        
     }
 });
 
-input.addEventListener('blur', function () {
-     this.parentElement.textContent = this.value;
-     this.classList.add('hidden');
-     document.body.appendChild(this);
+input.addEventListener('blur', function (e) {
+    // this.parentElement.textContent = this.value;
+    // this.classList.add('hidden');
+    // document.body.appendChild(this);
+
+    e.target.parentElement.textContent = input.value;
+    input.classList.add('hidden');
+    document.body.appendChild(input);
+    console.log(e.target);
     });
 
     // newRow
 
     newRow.addEventListener('click', function(e) {
         var newLine = document.createElement('div');
-            newLine.innerHTML = '<div class="center__item item">';
-            c.prepend(newLine);
+            newLine.innerHTML = '<div class="center__block"><div class="center__item item"></div><div class="center__item item"></div><div class="center__item item"></div></div>';
+            mainBlock.prepend(newLine);
     });
+    
+    // keyFocus
+
+    document.addEventListener('keypress', function(e) {
+        if(e.key == 'Enter') {
+            input.classList.add('hidden');
+            }
+    });
+
+
+
