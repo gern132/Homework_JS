ba8f2054aa0e871835324905907b860435f93654
Aleksandr Lazarev <aleqqq132@gmail.com>
2021-05-19 10:36:52 +0300

    revisions

--- Homework_15/script_.js
+++ Homework_15/script_.js
@@ -1,6 +1,7 @@
 var button = document.getElementById('button'),
     main = document.getElementById('main'),
-    mainBlock = document.querySelector('.chess');
+    mainBlock = document.querySelector('.chess'),
+    middle = document.querySelector('.middle__block');
 
     main.addEventListener('keyup', function (e) {
         var x = document.querySelector('.x'),
@@ -22,37 +23,52 @@ var button = document.getElementById('button'),
         });
     });
 
-    button.addEventListener('click', function (e) {
+    button.addEventListener('click', function res1() {
         var x = document.querySelector('.x').value,
-            y = document.querySelector('.y').value;
-            mainBlock.innerHTML = '';
-        function rrr(x,y) {
-            for(var i = 0; i < y; i++) {
-                for(var j = 0; j < x; j++) {
-                    if(i % 2 !== 0) {
-                        (j % 2 === 0) ? white() : black()
-                    } else {
-                        (j % 2 === 0) ? black() : white()
+                    y = document.querySelector('.y').value;
+                    mainBlock.innerHTML = '';
+                function chess(x,y) {
+                    for(var i = 0; i < y; i++) {
+                        for(var j = 0; j < x; j++) {
+                            if(i % 2 !== 0) {
+                                (j % 2 === 0) ? white() : black()
+                            } else {
+                                (j % 2 === 0) ? black() : white()
+                            }
+                        }
                     }
+                    var width = x * 72;
+                    mainBlock.style.width = width + 'px';
                 }
-            }
-            var width = x * 72;
-            mainBlock.style.width = width + 'px';
-        }
-        rrr(+x, +y)
-            });
-
+                chess(+x, +y)
+    });
             function white() {
-                var a = document.createElement('div');
+                a = document.createElement('div');
                 a.className = 'black';
                 mainBlock.append(a);
             }
             function black() {
-                var b = document.createElement('div');
+                b = document.createElement('div');
                 b.className = 'white';
                 mainBlock.append(b);
             }
-    
-
-    
-
+        
+    middle.addEventListener('click', function() {
+        var x = document.querySelector('.x').value,
+            y = document.querySelector('.y').value;
+            mainBlock.innerHTML = '';
+            function chess1(x,y) {
+                for(var i = 0; i < y; i++) {
+                    for(var j = 0; j < x; j++) {
+                        if(i % 2 !== 0) {
+                            (j % 2 === 0) ? black() : white()
+                        } else {
+                            (j % 2 === 0) ? white() : black()
+                        }
+                    }
+                }
+                var width = x * 72;
+                mainBlock.style.width = width + 'px';
+            }
+            chess1(+x, +y)
+    });
