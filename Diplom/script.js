ecf562a9b45f08dcf3acaf8b306a7d2c0b71dd2e
Aleksandr <gern5051@gmail.com>
2021-06-18 14:25:03 +0300

    v0.02

--- Diplom/script.js
+++ Diplom/script.js
@@ -6,34 +6,37 @@ let arr = [],
     block,
     b,
     p = document.getElementsByTagName('p'),
+    letters = document.getElementsByTagName('p'),
     pos,
     n;
+let counter = 0;
 
 button[0].addEventListener('click', () => {
     let mechanic = setInterval(function res() {
         arr.push(alfa.charAt(Math.floor(Math.random() * alfa.length)));
 
         b = document.createElement('p');
-        b.className = 'text';
+        b.className = 'text main__text';
         b.innerHTML = arr[arr.length - 1];
         main.append(b);
 
-        if(document.getElementsByTagName('p').length >= 100) clearInterval(mechanic);
-        // p = document.getElementsByTagName('p');
+        if(letters.length >= 100) clearInterval(mechanic);
 
     let animation = anime({
-        targets: '.text',
-        translateX: -1000,
+        targets: letters[counter++],
+        translateX: -2000,
         duration: 5000,
         easing: 'linear'
       });
-        }, 500);
 
-        let i = 0; //счёткич не нажатых клавиш
+    //   counter++
+        }, 1000);
+
+        let i = 0; //счёткич до lose
         let removeInterval = setInterval(() => {  //подсчёт конечных координат
-            Array.from(p).forEach(function(e) {
+            Array.from(allLetters).forEach(function(e) {
                 let position = e.getBoundingClientRect();
-                if(position.left < 130) {
+                if(position.left < 30) {
                     i++;
                     if(i >= 2) {
                         clearInterval(mechanic);
@@ -47,7 +50,7 @@ button[0].addEventListener('click', () => {
 });
 
     document.addEventListener('keypress' , function del(e) {
-            for(let i = 0; i < allLetters.length; i++) {
-                if(allLetters[i].textContent == e.key) allLetters[i].remove();
+            for(let i = 0; i < p.length; i++) {
+                if(p[i].textContent == e.key) p[i].classList.remove('text');
             }
         });
