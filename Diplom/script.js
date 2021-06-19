66695a06fb0836565d0b99aeaa8611f5c50ccdce
Aleksandr <gern5051@gmail.com>
2021-06-19 11:07:57 +0300

    v0.03

--- Diplom/script.js
+++ Diplom/script.js
@@ -1,8 +1,10 @@
 let button = document.getElementsByTagName('button'),
     canvas = document.getElementById('main');
 let alfa = 'abcdefghijklmnopqrstuvwxyz';   //abcdefghijklmnopqrstuvwxyz
-let arr = [],
+let arrRandom = [],
     allLetters = document.getElementsByClassName('text'),
+    textCanvas = document.querySelector('.text__block'),
+    bgPuzzle = document.getElementsByClassName('mini__anime'),
     block,
     b,
     p = document.getElementsByTagName('p'),
@@ -13,12 +15,12 @@ let counter = 0;
 
 button[0].addEventListener('click', () => {
     let mechanic = setInterval(function res() {
-        arr.push(alfa.charAt(Math.floor(Math.random() * alfa.length)));
+        arrRandom.push(alfa.charAt(Math.floor(Math.random() * alfa.length)));
 
         b = document.createElement('p');
         b.className = 'text main__text';
-        b.innerHTML = arr[arr.length - 1];
-        main.append(b);
+        b.innerHTML = arrRandom[arrRandom.length - 1];
+        textCanvas.append(b);
 
         if(letters.length >= 100) clearInterval(mechanic);
 
@@ -30,7 +32,7 @@ button[0].addEventListener('click', () => {
       });
 
     //   counter++
-        }, 1000);
+        }, 800);
 
         let i = 0; //счёткич до lose
         let removeInterval = setInterval(() => {  //подсчёт конечных координат
@@ -40,7 +42,16 @@ button[0].addEventListener('click', () => {
                     i++;
                     if(i >= 2) {
                         clearInterval(mechanic);
-                        canvas.innerHTML = '';
+                        textCanvas.innerHTML = '';
+
+                        anime({
+                            targets: '.mini__anime',
+                            scale: [
+                                {value: .1, easing: 'easeOutSine', duration: 500},
+                                {value: 1, easing: 'easeInOutQuad', duration: 1200}
+                              ],
+                              delay: anime.stagger(200, {grid: [21, 26], from: 'center'}) //[15, 9]
+                          });
                     }
                     console.log(i);
                     e.remove();
@@ -49,8 +60,13 @@ button[0].addEventListener('click', () => {
         }, 10);
 });
 
+let counterRemove = 0;
     document.addEventListener('keypress' , function del(e) {
-            for(let i = 0; i < p.length; i++) {
-                if(p[i].textContent == e.key) p[i].classList.remove('text');
-            }
+
+                if(p[counterRemove].textContent == e.key) {
+                    p[counterRemove].classList.remove('text');
+                    counterRemove++
+                }
         });
+
+       
