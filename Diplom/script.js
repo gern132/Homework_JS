71b678d4753f17b42138d774f48e1b86d7d00f9f
Aleksandr <gern5051@gmail.com>
2021-06-20 01:28:07 +0300

    v0.04

--- Diplom/script.js
+++ Diplom/script.js
@@ -12,6 +12,7 @@ let arrRandom = [],
     pos,
     n;
 let counter = 0;
+let i = 0; //счёткич до lose
 
 button[0].addEventListener('click', () => {
     let mechanic = setInterval(function res() {
@@ -22,28 +23,25 @@ button[0].addEventListener('click', () => {
         b.innerHTML = arrRandom[arrRandom.length - 1];
         textCanvas.append(b);
 
-        if(letters.length >= 100) clearInterval(mechanic);
-
     let animation = anime({
         targets: letters[counter++],
         translateX: -2000,
         duration: 5000,
         easing: 'linear'
       });
-
-    //   counter++
         }, 800);
 
-        let i = 0; //счёткич до lose
         let removeInterval = setInterval(() => {  //подсчёт конечных координат
             Array.from(allLetters).forEach(function(e) {
                 let position = e.getBoundingClientRect();
                 if(position.left < 30) {
                     i++;
                     if(i >= 2) {
+                        // arrRandom.splice(0, arrRandom[arrRandom.length - 1]);
+    
                         clearInterval(mechanic);
+                        clearInterval(removeInterval);
                         textCanvas.innerHTML = '';
-
                         anime({
                             targets: '.mini__anime',
                             scale: [
@@ -53,20 +51,27 @@ button[0].addEventListener('click', () => {
                               delay: anime.stagger(200, {grid: [21, 26], from: 'center'}) //[15, 9]
                           });
                     }
-                    console.log(i);
-                    e.remove();
+                    e.classList.remove('text');
                 }
             });
         }, 10);
 });
 
-let counterRemove = 0;
     document.addEventListener('keypress' , function del(e) {
 
-                if(p[counterRemove].textContent == e.key) {
-                    p[counterRemove].classList.remove('text');
-                    counterRemove++
+                if(allLetters[0].textContent == e.key) {
+                    allLetters[0].classList.remove('text');
                 }
         });
 
-       
+        // let a = [1, 2, 3];
+        // a.splice(0, a[a.length - 1]);
+        // console.log(a);
+
+    var y = [8];
+            
+        function o() {
+           var y = [2];
+        }
+
+        console.log(y);
